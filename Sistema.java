10d516cb3a652d1ec5f84c53f2b0a746b5fc00a0
Cassiano Abreu <cassiano.abreuu@gmail.com>
2026-09-05 18:52:22 -0300

    REFACTOR[1] - Melhorias de organização e modularização do código
    Melhorias realizadas: nomes de variáveis mais descritivos, separação em métodos com responsabilidades específicas e adição de comentários explicativos.

--- Sistema.java
+++ Sistema.java
@@ -1,14 +1,29 @@
 public class Sistema {
+    
     public static void main(String[] args) {
-        String n = "Carlos";
-        double a = 8;
-        double b = 7;
-        double c = (a + b) / 2;
+        String nomeAluno = "Carlos";
+        double nota1 = 8;
+        double nota2 = 7;
+        double media = calcularMedia(nota1, nota2);
         
-        System.out.println("Aluno: " + n);
-        System.out.println("Media: " + c);
-        
-        if (c >= 6) {
+        exibirResultados(nomeAluno, media);
+        verificarSituacao(media);
+    }
+    
+    // Calcula a média entre duas notas
+    public static double calcularMedia(double nota1, double nota2) {
+        return (nota1 + nota2) / 2;
+    }
+    
+    // Exibe o nome do aluno e sua média
+    public static void exibirResultados(String nomeAluno, double media) {
+        System.out.println("Aluno: " + nomeAluno);
+        System.out.println("Media: " + media);
+    }
+    
+    // Verifica se o aluno foi aprovado ou reprovado
+    public static void verificarSituacao(double media) {
+        if (media >= 6) {
             System.out.println("Aprovado");
         } else {
             System.out.println("Reprovado");
